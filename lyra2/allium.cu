35dac3ad1b99c1787b905a4c7b733db24179d188
tuxprint <42660178+tuxprint@users.noreply.github.com>
2018-09-15 11:22:56 -0500

    modified alluim algo

--- lyra2/allium.cu
+++ lyra2/allium.cu
@@ -10,7 +10,7 @@ extern "C" {
 #include <miner.h>
 #include <cuda_helper.h>
 
-static uint64_t* d_hash[MAX_GPUS];
+static uint64_t *d_hash[MAX_GPUS];
 static uint64_t* d_matrix[MAX_GPUS];
 
 extern void blake256_cpu_init(int thr_id, uint32_t threads);
@@ -29,8 +29,8 @@ extern void cubehash256_cpu_hash_32(int thr_id, uint32_t threads, uint32_t start
 
 extern void skein256_cpu_init(int thr_id, uint32_t threads);
 
-extern void lyra2_cpu_init(int thr_id, uint32_t threads, uint64_t *d_matrix);
-extern void lyra2_cpu_hash_32(int thr_id, uint32_t threads, uint64_t *d_outputHash, bool gtx750ti);
+extern void lyra2v2_cpu_hash_32(int thr_id, uint32_t threads, uint32_t startNonce, uint64_t *d_outputHash, int order);
+extern void lyra2v2_cpu_init(int thr_id, uint32_t threads, uint64_t* d_matrix);
 
 extern void groestl256_cpu_init(int thr_id, uint32_t threads);
 extern void groestl256_cpu_free(int thr_id);
@@ -38,8 +38,7 @@ extern void groestl256_setTarget(const void *ptarget);
 extern uint32_t groestl256_cpu_hash_32(int thr_id, uint32_t threads, uint32_t startNounce, uint64_t *d_outputHash, int order);
 extern uint32_t groestl256_getSecNonce(int thr_id, int num);
 
-
-extern "C" void allium_hash(void *state, const void *input)
+void allium_hash(void *state, const void *input)
 {
 	uint32_t hashA[8], hashB[8];
 
@@ -59,13 +58,13 @@ extern "C" void allium_hash(void *state, const void *input)
 	sph_keccak256(&ctx_keccak, hashA, 32);
 	sph_keccak256_close(&ctx_keccak, hashB);
 
-	LYRA2(hashA, 32, hashB, 32, hashB, 32, 1, 8, 8);
+	LYRA2(hashA, 32, hashB, 32, hashB, 32, 1, 4, 4);
 
 	sph_cubehash256_init(&ctx_cube);
 	sph_cubehash256(&ctx_cube, hashA, 32);
 	sph_cubehash256_close(&ctx_cube, hashB);
 
-	LYRA2(hashA, 32, hashB, 32, hashB, 32, 1, 8, 8);
+	LYRA2(hashA, 32, hashB, 32, hashB, 32, 1, 4, 4);
 
 	sph_skein256_init(&ctx_skein);
 	sph_skein256(&ctx_skein, hashA, 32);
@@ -79,60 +78,54 @@ extern "C" void allium_hash(void *state, const void *input)
 }
 
 static bool init[MAX_GPUS] = { 0 };
-static __thread uint32_t throughput = 0;
 
 extern "C" int scanhash_allium(int thr_id, struct work* work, uint32_t max_nonce, unsigned long *hashes_done)
 {
 	uint32_t *pdata = work->data;
 	uint32_t *ptarget = work->target;
 	const uint32_t first_nonce = pdata[19];
+	int dev_id = device_map[thr_id];
+	int intensity = (device_sm[dev_id] < 500) ? 18 : is_windows() ? 19 : 20;
+	if (strstr(device_name[dev_id], "GTX 10")) intensity = 20;
+	uint32_t throughput = cuda_default_throughput(dev_id, 1UL << intensity);
+	if (init[thr_id]) throughput = min(throughput, max_nonce - first_nonce);
 
 	if (opt_benchmark)
-		ptarget[7] = 0x00ff;
+		ptarget[7] = 0x000f;
 
-	static __thread bool gtx750ti;
 	if (!init[thr_id])
 	{
-		int dev_id = device_map[thr_id];
+		size_t matrix_sz = 16 * sizeof(uint64_t) * 4 * 3;
 		cudaSetDevice(dev_id);
 		if (opt_cudaschedule == -1 && gpu_threads == 1) {
 			cudaDeviceReset();
+			// reduce cpu usage
 			cudaSetDeviceFlags(cudaDeviceScheduleBlockingSync);
 			CUDA_LOG_ERROR();
 		}
-
-		int intensity = (device_sm[dev_id] >= 500 && !is_windows()) ? 17 : 16;
-		if (device_sm[device_map[thr_id]] == 500) intensity = 15;
-		throughput = cuda_default_throughput(thr_id, 1U << intensity); // 18=256*256*4;
-		if (init[thr_id]) throughput = min(throughput, max_nonce - first_nonce);
-
-		cudaDeviceProp props;
-		cudaGetDeviceProperties(&props, dev_id);
-
-		if (strstr(props.name, "750 Ti")) gtx750ti = true;
-		else gtx750ti = false;
-
 		gpulog(LOG_INFO, thr_id, "Intensity set to %g, %u cuda threads", throughput2intensity(throughput), throughput);
 
 		blake256_cpu_init(thr_id, throughput);
-		//keccak256_sm3_init(thr_id, throughput);
+		//keccak256_sm3_init(thr_id,throughput);
 		skein256_cpu_init(thr_id, throughput);
 		groestl256_cpu_init(thr_id, throughput);
 
-		//cuda_get_arch(thr_id);
-		if (device_sm[dev_id] >= 500)
-		{
-			size_t matrix_sz = device_sm[dev_id] > 500 ? sizeof(uint64_t) * 4 * 4 : sizeof(uint64_t) * 8 * 8 * 3 * 4;
-			CUDA_SAFE_CALL(cudaMalloc(&d_matrix[thr_id], matrix_sz * throughput));
-			lyra2_cpu_init(thr_id, throughput, d_matrix[thr_id]);
-		}
+		cuda_get_arch(thr_id); // cuda_arch[] also used in cubehash256
+
+		// SM 3 implentation requires a bit more memory
+		if (device_sm[dev_id] < 500 || cuda_arch[dev_id] < 500)
+			matrix_sz = 16 * sizeof(uint64_t) * 4 * 4;
+			
+		CUDA_SAFE_CALL(cudaMalloc(&d_matrix[thr_id], matrix_sz * throughput));
+		lyra2v2_cpu_init(thr_id, throughput, d_matrix[thr_id]);
 
 		CUDA_SAFE_CALL(cudaMalloc(&d_hash[thr_id], (size_t)32 * throughput));
 
+		api_set_throughput(thr_id, throughput);
 		init[thr_id] = true;
 	}
 
-	uint32_t _ALIGN(128) endiandata[20];
+	uint32_t endiandata[20];
 	for (int k=0; k < 20; k++)
 		be32enc(&endiandata[k], pdata[k]);
 
@@ -145,11 +138,12 @@ extern "C" int scanhash_allium(int thr_id, struct work* work, uint32_t max_nonce
 		//blake256_cpu_hash_80(thr_id, throughput, pdata[19], d_hash[thr_id], order++);
 		//keccak256_sm3_hash_32(thr_id, throughput, pdata[19], d_hash[thr_id], order++);
 		blakeKeccak256_cpu_hash_80(thr_id, throughput, pdata[19], d_hash[thr_id], order++);
-		lyra2_cpu_hash_32(thr_id, throughput, d_hash[thr_id], gtx750ti);
+		lyra2v2_cpu_hash_32(thr_id, throughput, pdata[19], d_hash[thr_id], order++);
 		cubehash256_cpu_hash_32(thr_id, throughput, pdata[19], d_hash[thr_id], order++);
-		lyra2_cpu_hash_32(thr_id, throughput, d_hash[thr_id], gtx750ti);
+		lyra2v2_cpu_hash_32(thr_id, throughput, pdata[19], d_hash[thr_id], order++);
 		skein256_cpu_hash_32(thr_id, throughput, pdata[19], d_hash[thr_id], order++);
 
+		memset(work->nonces, 0, sizeof(work->nonces));
 		*hashes_done = pdata[19] - first_nonce + throughput;
 
 		work->nonces[0] = groestl256_cpu_hash_32(thr_id, throughput, pdata[19], d_hash[thr_id], order++);
@@ -208,8 +202,9 @@ extern "C" void free_allium(int thr_id)
 	cudaFree(d_hash[thr_id]);
 	cudaFree(d_matrix[thr_id]);
 
+	//bmw256_cpu_free(thr_id);
 	//keccak256_sm3_free(thr_id);
-	groestl256_cpu_free(thr_id);
+        groestl256_cpu_free(thr_id);
 
 	init[thr_id] = false;
 
